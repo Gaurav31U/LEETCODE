6b456b80c9ccf59ebd0c934b6737f0b4480ceb66
Gaurav <gaurav319288@gmail.com>
2024-08-28 23:10:46 +0530

    Time: 85 ms (97.24%), Space: 91.5 MB (6.44%) - LeetHub

---
+++ 1305-number-of-visible-people-in-a-queue/1305-number-of-visible-people-in-a-queue.cpp
@@ -0,0 +1,32 @@
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+class Solution {
+public:
+    vector<int> canSeePersonsCount(vector<int>& heights) {
+        vector<int> ans;
+        int cnt=0;
+        ans.push_back(0);
+        vector<int> st;
+        st.push_back(heights.back());
+        int prev=heights.back();
+        for(int i=heights.size()-2;i>=0;i--){
+            if(prev>heights[i])ans.push_back(1);
+            else{
+                int nums=lower_bound(st.rbegin(),st.rend(),heights[i])-st.rbegin()+1;
+                nums=min((int)st.size(),nums);
+                ans.push_back(nums);
+            } 
+            while(!st.empty() && st.back()<heights[i]){
+                st.pop_back();
+            }
+            st.push_back(heights[i]);
+            prev=heights[i];
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
