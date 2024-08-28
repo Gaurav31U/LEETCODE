7ff2fb336a2eed7074927d92ba119212ed984369
Gaurav <gaurav319288@gmail.com>
2024-08-28 22:46:41 +0530

    Time: 504 ms (15.02%), Space: 184.9 MB (20.58%) - LeetHub

---
+++ 1723-maximum-number-of-achievable-transfer-requests/1723-maximum-number-of-achievable-transfer-requests.cpp
@@ -0,0 +1,28 @@
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+class Solution {
+public:
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        int ans=0;
+        int m=requests.size();
+        for(int i=0;i<(1<<m);i++){
+            vector<int> ind(n,0);
+            int cnt=0;
+            for(int j=0;j<m;j++){
+                if((1<<j)&i){
+                    ind[requests[j][0]]--;
+                    ind[requests[j][1]]++;
+                    cnt++;
+                }
+            }
+            int s=0;
+            for(auto it:ind)s+=abs(it);
+            if(s==0)ans=max(ans,cnt);
+        }
+        return ans;
+    }
+};
