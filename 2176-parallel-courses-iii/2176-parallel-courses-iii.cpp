0225bd99c1050c5b38805a90b92fd25d28f1ca69
Gaurav <gaurav319288@gmail.com>
2024-08-28 23:07:41 +0530

    Time: 293 ms (55.99%), Space: 145.5 MB (52.84%) - LeetHub

---
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
@@ -0,0 +1,30 @@
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+class Solution {
+public:
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        vector<int> adj[50001];
+        int ind[50001];
+        memset(ind,0,sizeof(ind));
+        for(auto it:relations){
+            ind[it[1]]++;
+            adj[it[0]].push_back(it[1]);
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
+        for(int i=1;i<=n;i++)if(ind[i]==0)q.push({time[i-1],i});
+        int s=0;
+        while(!q.empty()){
+            auto tp=q.top();
+            q.pop();
+            s=max(s,tp.first);
+            for(auto it:adj[tp.second])
+                if(--ind[it]==0)
+                    q.push({tp.first+time[it-1],it}); 
+        }    
+        return s;
+    }
+};
