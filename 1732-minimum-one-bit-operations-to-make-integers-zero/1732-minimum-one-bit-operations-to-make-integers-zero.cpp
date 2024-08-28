33e43a748f8cd38e5c23b96c8b99092d5e41c138
Gaurav <gaurav319288@gmail.com>
2024-08-28 18:14:19 +0530

    Time: 3 ms (37.07%), Space: 7.8 MB (27.74%) - LeetHub

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/1732-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,33 @@
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        if(n==0)return 0;
+        int dp[32];
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+        dp[1]=3;
+        dp[2]=7;
+        for(int i=3;i<30;i++){
+            dp[i]+=2*dp[i-1];
+            dp[i]++;
+        }
+        int ans=0;
+        bool ok=true;
+        for(int i=0;i<30;i++){
+            if((1<<i)&n){
+                if(ok)ans-=(dp[i]);
+                else ans+=dp[i];
+                ok^=1;
+            }
+        }
+        return abs(ans);
+
+
+    }
+};
