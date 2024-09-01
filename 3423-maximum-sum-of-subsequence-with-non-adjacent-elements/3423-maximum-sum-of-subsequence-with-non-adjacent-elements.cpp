35822dcd00788b3841c60cdb43f69b847b9483e5
Gaurav <gaurav319288@gmail.com>
2024-09-02 00:03:53 +0530

    Time: 247 ms (98.38%), Space: 144.5 MB (75.65%) - LeetHub

--- 3423-maximum-sum-of-subsequence-with-non-adjacent-elements/3423-maximum-sum-of-subsequence-with-non-adjacent-elements.cpp
+++ 3423-maximum-sum-of-subsequence-with-non-adjacent-elements/3423-maximum-sum-of-subsequence-with-non-adjacent-elements.cpp
@@ -1,61 +1,37 @@
-const long long inf = 1e18;
-const long long mod = 1e9 + 7;
-struct Node {
-    Node *l = nullptr, *r = nullptr;
-    unsigned int lo, hi;
-    long long selected[2][2];
-
-    Node(const vector<int> &nums, unsigned int lo, unsigned int hi) : lo(lo), hi(hi) {
-        if (lo < hi) {
-            unsigned int mid = (lo + hi) / 2;
-            l = new Node(nums, lo, mid);
-            r = new Node(nums, mid + 1, hi);
-
-            combine();
-        } else {
-            selected[0][0] = 0;
-            selected[0][1] = -inf;
-            selected[1][0] = -inf;
-            selected[1][1] = nums[lo];
-        }
-    }
-
-    void combine() {
-        selected[0][0] = max(l->selected[0][0] + r->selected[0][0], max(l->selected[0][1] + r->selected[0][0], l->selected[0][0] + r->selected[1][0]));
-        selected[0][1] = max(l->selected[0][0] + r->selected[0][1], max(l->selected[0][1] + r->selected[0][1], l->selected[0][0] + r->selected[1][1]));
-        selected[1][0] = max(l->selected[1][0] + r->selected[0][0], max(l->selected[1][1] + r->selected[0][0], l->selected[1][0] + r->selected[1][0]));
-        selected[1][1] = max(l->selected[1][0] + r->selected[0][1], max(l->selected[1][1] + r->selected[0][1], l->selected[1][0] + r->selected[1][1]));
-    }
-
-    void update(int i, long long x) {
-        if (i < lo || hi < i) {
-            return;
-        }
-
-        if (lo == hi) {
-            selected[0][0] = 0;
-            selected[1][1] = x;
-            return;
-        }
-
-        l->update(i, x);
-        r->update(i, x);
-
-        combine();
-    }
-};
+constexpr int MOD = 1e9 + 7;
 
 class Solution {
-  public:
-    int maximumSumSubsequence(vector<int> &nums, vector<vector<int>> &queries) {
-        Node segmentTree(nums, 0, nums.size() - 1);
-
-        long long res = 0;
-        for (auto &query : queries) {
-            segmentTree.update(query[0], query[1]);
-            res += max(segmentTree.selected[0][0], max(segmentTree.selected[0][1], max(segmentTree.selected[1][0], segmentTree.selected[1][1])));
-            res %= mod;
+public:
+    int maximumSumSubsequence(vector<int>& nums, vector<vector<int>>& queries) {
+        const int n = nums.size();
+        int sz = 1;
+        while (sz < n) sz <<= 1;
+        vector<array<long, 4>> tree(sz << 1, {0, 0, 0, 0});
+        for (int i = 0; i < n; ++i) tree[sz + i][0] = max(nums[i], 0);
+        for (int i = n; i < sz; ++i) tree[sz + i][0] = LONG_MIN;
+        for (int i = sz - 1; i > 0; --i) merge(tree[i], tree[i << 1], tree[(i << 1) + 1]);
+        long res = 0ll;
+        for (const auto& q : queries) {
+            int i = q[0] + sz;
+            tree[i][0] = max(q[1], 0);
+            while (i > 1) {
+                i >>= 1;
+                merge(tree[i], tree[i << 1], tree[(i << 1) + 1]);
+            }
+            res = (res + tree[1][0]) % MOD;
         }
-        return static_cast<int>(res);
+        return res;
+    }
+
+private:
+    void merge(array<long, 4>& dest, const array<long, 4>& a, const array<long, 4>& b) {
+        if (b[0] == LONG_MIN) {
+            dest = a;
+            return;
+        }
+        dest[0] = max(a[2] + b[0], a[0] + b[1]);
+        dest[1] = max(a[3] + b[0], a[1] + b[1]);
+        dest[2] = max(a[0] + b[3], a[2] + b[2]);
+        dest[3] = max(a[3] + b[2], a[1] + b[3]);
     }
 };
