c3dde4e1e52a467b41ef33a3b50983db29266238
Gaurav <gaurav319288@gmail.com>
2024-09-02 15:21:03 +0530

    Time: 11 ms (87.42%), Space: 9 MB (71.78%) - LeetHub

--- 3017-number-of-beautiful-integers-in-the-range/3017-number-of-beautiful-integers-in-the-range.cpp
+++ 3017-number-of-beautiful-integers-in-the-range/3017-number-of-beautiful-integers-in-the-range.cpp
@@ -1,32 +1,40 @@
-vector<int> temp;
 class Solution {
 public:
-    bool chk(int n){
-        int c=0;
-        while(n>0){
-            if(n&1)c++;
-            else c--;
-            n/=10;
+    int dp[11][21][50][2][2];
+    int solve(string& num, int n, int diff, int sum, int k, bool leading, bool tight){
+        if(n == 0){
+            if(diff==0 && sum == k) return 1;
+            else return 0;
         }
-        return c==0;
+        if(dp[n][10+diff][sum][leading][tight] != -1) return dp[n][10+diff][sum][leading][tight];
+        int ans = 0;
+        int ub = tight ? num[num.size()-n]-'0' : 9;
+        for(int dig = 0; dig<=ub; dig++){
+            if(dig == 0 && leading == 1){
+                ans += solve(num, n-1, diff, (sum%k)+k, k, 1, 0);
+            }
+            else{
+                int val = 1;
+                for(int i=0; i<n-1; i++){
+                    val *= 10;
+                }
+                if(dig%2){
+                    ans += solve(num, n-1, diff+1, ((sum+(dig*val))%k)+k, k, 0, (tight&(ub==dig)));
+                }
+                else{
+                    ans += solve(num, n-1, diff-1, ((sum+(dig*val))%k)+k, k, 0, (tight&(ub==dig)));
+                }
+            }
+        }
+        return dp[n][10+diff][sum][leading][tight] = ans;
     }
-    int numberOfBeautifulIntegers(int low, int high, int k) {
-        //2 4 6 8
-        if(temp.empty()){
-            for(int i=10;i<=99;i++)
-                if(chk(i))temp.push_back(i);
-            for(int i=1000;i<=9999;i++)
-                if(chk(i))temp.push_back(i);
-            for(int i=100000;i<=999999;i++)
-                if(chk(i))temp.push_back(i);
-            for(int i=10000000;i<=99999999;i++)
-                if(chk(i))temp.push_back(i);
-        }
-        int ans=0;
-        auto it=lower_bound(temp.begin(),temp.end(),low);
-        for(;it!=temp.end() && *it<=high;it++){
-            if((*it)%k==0)ans++;
-        }
-        return ans;
+    int numberOfBeautifulIntegers(int low, int high, int k) {   
+        string l = to_string(low-1);
+        string r = to_string(high);
+        memset(dp, -1, sizeof(dp));
+        int a = solve(l, l.size(), 0, 0, k, 1, 1);
+        memset(dp, -1, sizeof(dp));
+        int b = solve(r, r.size(), 0, 0, k, 1, 1);
+        return b-a;
     }
 };
