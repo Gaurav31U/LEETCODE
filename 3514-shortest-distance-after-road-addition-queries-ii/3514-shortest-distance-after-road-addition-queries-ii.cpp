0b73ce87dbccb1ec3813cbc101b850c7df7ae5ba
Gaurav <gaurav319288@gmail.com>
2024-09-04 14:48:47 +0530

    Time: 723 ms (6.6%), Space: 218.3 MB (78.48%) - LeetHub

---
+++ 3514-shortest-distance-after-road-addition-queries-ii/3514-shortest-distance-after-road-addition-queries-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
+        vector<int> ans(queries.size());
+        map<int,int> mp;
+        for(int i=0;i<n-1;i++){
+            mp[i]=i+1;
+        }
+        cout<<mp.size();
+        for(int i=0;i<queries.size();i++){
+            int u=queries[i][0];
+            int v=queries[i][1];
+            if(!mp.count(u) || mp[u]>v){
+                ans[i]=mp.size();
+                continue;
+            }
+            int j=mp[u];
+            while(j<v){
+                int t=j;
+                j=mp[j];
+                mp.erase(t);
+            }
+            mp[u]=v;
+            ans[i]=mp.size();
+        }
+        return ans;
+    }
+};
