1686560cd819f00643462d7712e49617f863feb5
Gaurav <gaurav319288@gmail.com>
2024-09-05 15:44:46 +0530

    Time: 352 ms (63.18%), Space: 32.1 MB (61.09%) - LeetHub

---
+++ 2647-split-the-array-to-make-coprime-products/2647-split-the-array-to-make-coprime-products.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int prime[1000001];
+    int findValidSplit(vector<int>& nums) {
+        if(nums.size()==1)return -1;
+        for(int i=0;i<=1e6;i++)prime[i]=i;
+        for(int i=2;i<=1e3;i++){
+            if(prime[i]==i){
+                for(int j=i*i;j<=1e6;j+=i)
+                    prime[j]=i;
+            }
+        }
+        map<int,int> temp;
+        for(int i=nums.size()-1;i>=0;i--){
+            int t=nums[i];
+            // if(t==1)continue;
+            while(t>1 && prime[t]!=t){
+                int p=prime[t];
+                temp[p]++;
+                t/=p;
+            }
+            if(t>1)temp[t]++;
+        }
+        unordered_set<int> st;
+        int cnt=0;
+        for(int i=0;i<nums.size()-1;i++){
+            int t=nums[i];
+            // if(t==1)continue;
+            while(t>1 && prime[t]!=t){
+                int p=prime[t];
+                st.insert(p);
+                if(--temp[p]==0)cnt++;
+                t/=p;
+            }
+            if(t>1){
+                temp[t]--;
+                if(temp[t]==0)cnt++;
+                st.insert(t);
+            }
+            if(st.size()==cnt)return i;
+        }
+        return -1;
+    }
+};
