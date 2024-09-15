b7f1b9c841e436ce670bd7efbb9155a9ae6696a8
Gaurav <gaurav319288@gmail.com>
2024-09-15 16:03:40 +0530

    Time: 1827 ms (8.01%), Space: 268.2 MB (13.08%) - LeetHub

---
+++ 0823-split-array-with-same-average/0823-split-array-with-same-average.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool splitArraySameAverage(vector<int>& nums) {
+        int n=nums.size();
+        vector<unordered_set<int>> tmp(n/2+1);
+        tmp[0].insert(0);
+        for(auto num:nums)
+            for(int i=n/2;i>=1;i--)
+                for(auto it:tmp[i-1])
+                    tmp[i].insert(it+num);
+        
+        int sum=0;
+        for(auto it:nums)sum+=it;
+        for(int i=1;i<=n/2;i++){
+            if(i*sum%n==0){
+                if(tmp[i].find(i*sum/n)!=tmp[i].end())return true;
+            }
+        }
+        return false;
+    }
+};
