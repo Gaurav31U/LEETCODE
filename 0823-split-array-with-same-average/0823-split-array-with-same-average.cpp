23b757d83426924c6245855003acbde5a7d85269
Gaurav <gaurav319288@gmail.com>
2024-09-15 16:09:51 +0530

    Time: 64 ms (82.28%), Space: 19.2 MB (88.19%) - LeetHub

--- 0823-split-array-with-same-average/0823-split-array-with-same-average.cpp
+++ 0823-split-array-with-same-average/0823-split-array-with-same-average.cpp
@@ -7,16 +7,13 @@ public:
         for(int i=1;i<=n;i++){
             for(int j=sum-a[i-1];j>=0;j--){
                 if(dp[j]==0)continue;
-                int t=dp[j];
                 dp[j+a[i-1]]|=dp[j]<<1;
             }
             dp[a[i-1]]|=1;
         }
-        for(int len=1; len<n; len++) {
-            if( (sum*len)%n == 0 && ((1<<(len-1)) & dp[sum*len/n])) {
+        for(int len=1; len<n; len++) 
+            if( (sum*len)%n == 0 && ((1<<(len-1)) & dp[sum*len/n])) 
                 return true;
-            }
-        }
         return false;
     }
 };
