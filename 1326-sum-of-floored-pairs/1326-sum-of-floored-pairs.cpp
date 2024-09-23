66712a403a578fe100e72cd427147bb8928415c3
Gaurav <gaurav319288@gmail.com>
2024-09-23 07:19:14 +0530

    Time: 486 ms (41.51%), Space: 238.2 MB (5.66%) - LeetHub

--- 1326-sum-of-floored-pairs/1326-sum-of-floored-pairs.cpp
+++ 1326-sum-of-floored-pairs/1326-sum-of-floored-pairs.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int sumOfFlooredPairs(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        vector<int>arr(200005,0);
+        vector<long long>arr(200005,0);
         map<int,int>mp;
         for(auto it:nums){
-            arr[it]++;
             mp[it]++;
         }
-        for(int i=1;i<200005;i++)arr[i]+=arr[i-1];
-        long long ans=0;
         for(auto it:mp){
-            int j=1;
-            long long sum=0;
-            for(int i=it.first+it.first;i<200005;i+=it.first){
-                sum+=(arr[i-1]-arr[i-it.first-1])*j;
-                sum%=1000000007;
-                j++;
-            }
-            ans+=(sum*it.second);
-            ans%=1000000007;
+            for(int i=it.first;i<200005;i+=it.first)
+                arr[i]+=it.second;
         }
+        for(int i=1;i<200005;i++){
+            arr[i]+=arr[i-1];
+        }
+        long long ans=0;
+        int mod=1e9+7;
+        for(auto it:nums)(ans+=arr[it])%=mod;
         return ans;
     }
 };
