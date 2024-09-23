df527f1e3749787b3a99c63854852e55eef29171
Gaurav <gaurav319288@gmail.com>
2024-09-23 06:45:07 +0530

    Time: 97 ms (71.45%), Space: 71.4 MB (67.74%) - LeetHub

---
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        unordered_map<string,int> mp;
+        for(auto it:dictionary)mp[it]++;
+        int dp[101];
+        memset(dp,-1,sizeof(dp));
+        auto get=[&](auto&& get,int i)->int{
+            if(i>=s.size())return 0;
+            if(dp[i]!=-1)return dp[i];
+            string tmp="";
+            int ans=INT_MAX/2;
+            ans=min(ans,1+get(get,i+1));
+            for(int j=i;j<s.size();j++){
+                tmp+=s[j];
+                if(mp.count(tmp))
+                    ans=min(ans,get(get,j+1));
+            }
+
+            return dp[i]=ans;
+        };
+        return get(get,0);
+    }
+};
