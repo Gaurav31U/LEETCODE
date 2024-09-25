739474dba52787415286f7911e19cc27804efc82
Gaurav <gaurav319288@gmail.com>
2024-09-26 03:51:36 +0530

    Time: 809 ms (38.2%), Space: 176.6 MB (90.26%) - LeetHub

---
+++ 2818-maximum-strictly-increasing-cells-in-a-matrix/2818-maximum-strictly-increasing-cells-in-a-matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maxIncreasingCells(vector<vector<int>>& mat) {
+        vector<array<int,3>> tmp;
+        int n=mat.size(),m=mat[0].size();
+        int cnt=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                tmp.push_back({mat[i][j],i,j});
+            }
+        }
+        sort(tmp.begin(),tmp.end(),greater<array<int,3>>());
+        int dp[n][m];
+        memset(dp,-1,sizeof(dp));
+        int ans=1,f=0;
+        vector<priority_queue<int>> row(n),col(m);
+        vector<pair<int,int>> put_row,put_col;
+        int last=tmp[0][0];
+        for(auto it:tmp){
+            int i=it[1],j=it[2];
+            int val=it[0];
+            if(last!=val){
+                for(auto it:put_row)
+                    row[it.first].push(it.second);
+                for(auto it:put_col)
+                    col[it.first].push(it.second);
+                last=val;
+                put_row.clear();
+                put_col.clear();
+            }
+            cout<<last<<" ";
+            int mx=0;
+            if(!row[i].empty())mx=row[i].top();
+            if(!col[j].empty())mx=max(col[j].top(),mx);
+            dp[i][j]=max(dp[i][j],mx+1);
+            put_row.push_back({i,dp[i][j]});
+            put_col.push_back({j,dp[i][j]});
+            ans=max(ans,dp[i][j]);
+        }
+        return ans;
+    }
+};
