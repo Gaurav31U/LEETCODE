e4aedb856f57c9d55494fed9dc0fbcc34dbe7d5b
Gaurav <gaurav319288@gmail.com>
2024-09-27 01:16:40 +0530

    Time: 822 ms (44.66%), Space: 356.2 MB (46.6%) - LeetHub

--- 3032-maximize-value-of-function-in-a-ball-passing-game/3032-maximize-value-of-function-in-a-ball-passing-game.cpp
+++ 3032-maximize-value-of-function-in-a-ball-passing-game/3032-maximize-value-of-function-in-a-ball-passing-game.cpp
@@ -7,27 +7,21 @@ public:
         int ub = log2(k);
         vector<vector<int>> last_receiver(n,vector<int>(ub+1,0));
         vector<vector<ll>> sum(n,vector<ll>(ub+1,0));
-        for(int i = 0 ; i < n ; ++i)
-        {
+        for(int i = 0 ; i < n ; ++i){
             last_receiver[i][0] = v[i];
             sum[i][0] = i;
         }
-        for(int j = 1 ; j <= ub ; ++j)
-        {
-            for(int i = 0 ; i < n ; ++i)
-            {
+        for(int j = 1 ; j <= ub ; ++j){
+            for(int i = 0 ; i < n ; ++i){
                 last_receiver[i][j] = last_receiver[last_receiver[i][j-1]][j-1];
                 sum[i][j] = sum[i][j-1] + sum[last_receiver[i][j-1]][j-1]; 
             }
         } 
-        for(int i = 0 ; i < n ; ++i)
-        {
+        for(int i = 0 ; i < n ; ++i){
             ll op(0);
             int x = i;
-            for(int j = 0 ; j <= ub ; ++j)
-            {
-                if((1ll<<j)&k)
-                {
+            for(int j = 0 ; j <= ub ; ++j){
+                if((1ll<<j)&k){
                     op += sum[x][j];
                     x = last_receiver[x][j];
                 }
