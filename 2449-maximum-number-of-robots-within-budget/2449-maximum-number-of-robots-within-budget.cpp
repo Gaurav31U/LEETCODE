f79433a09921d0949933c8dca51ccb3aa3594588
Gaurav <gaurav319288@gmail.com>
2024-10-11 00:32:58 +0530

    Time: 2694 ms (5.2%), Space: 448.6 MB (5.98%) - LeetHub

---
+++ 2449-maximum-number-of-robots-within-budget/2449-maximum-number-of-robots-within-budget.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
+        // Binary Search
+        int n=chargeTimes.size();
+        int l=1,r=n;
+        int ans=0;
+        auto solve=[&](int m)->bool{
+            multiset<int,greater<int>> st;
+            long long b=0;
+            bool found=false;
+            for(int i=0;i<m;i++){
+                b+=(m*1ll*runningCosts[i]);
+                st.insert(chargeTimes[i]);
+            }
+            if(*st.begin()+b<=budget){
+                found=true;
+            }
+            for(int i=m;i<n;i++){
+                st.erase(st.find(chargeTimes[i-m]));
+                st.insert(chargeTimes[i]);
+                b-=m*1ll*runningCosts[i-m];
+                b+=m*1ll*runningCosts[i];
+                if(*st.begin()+b<=budget){
+                    found=true;
+                }
+            }
+            return found;
+        };
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(solve(mid)){
+                ans=max(ans,mid);
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+};
