38ba5a69e47d43e815b421d8fb23c0c1961fbed1
Gaurav <gaurav319288@gmail.com>
2024-10-14 19:00:57 +0530

    Time: 2 ms (79.69%), Space: 7.9 MB (87.24%) - LeetHub

---
+++ 3414-find-number-of-ways-to-reach-the-k-th-stair/3414-find-number-of-ways-to-reach-the-k-th-stair.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    unsigned long long binomialCoefficient(int n, int r) {
+        if (r > n) {
+            return 0;  
+        }
+        if (r == 0 || r == n) {
+            return 1;  
+        }
+        r = min(r, n - r); 
+        unsigned long long result = 1;
+        for (int i = 1; i <= r; ++i) {
+            result = result * (n - r + i) / i;
+        }
+        return result;
+    }
+    int waysToReachStair(int k) {
+        if( k == 0 ) return 2;
+        if( k == 1 ) return 4;
+        if( k == 2 ) return 4;
+        int bit = __builtin_popcount(k);
+        int count = 0;
+        int l = k;
+        while(l) {
+            count++;
+            l = l >> 1;
+        }  
+        l=count;
+        int u = 1LL<<l;
+        int n = l + 1;
+        int r = u - k;
+        int ans = binomialCoefficient(n,r)  ;
+        return ans + ( bit == 1 ? 1 : 0 ) ;
+    }
+};
