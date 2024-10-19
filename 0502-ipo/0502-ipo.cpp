117181a9cbbf3e5a79007068ce02fc82c9b484ea
Gaurav <gaurav319288@gmail.com>
2024-10-19 08:46:50 +0530

    Time: 86 ms (100%), Space: 135.8 MB (10.37%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        priority_queue<long long> pq; 
+        vector<pair<int,int>> temp;
+        for(int i=0;i<profits.size();i++){
+            temp.push_back({capital[i],profits[i]});
+        }
+        sort(temp.begin(),temp.end());
+        for(int i=0;i<temp.size();i++){
+            if(temp[i].first<=w){
+                pq.push(temp[i].second);
+            }else{
+                if(pq.empty())return w;
+                w+=pq.top();
+                pq.pop();
+                k--;
+                while(!pq.empty() && w<temp[i].first && k>0){
+                    w+=pq.top();
+                    pq.pop();
+                    k--;
+                }
+                if(k==0)return w;
+                if(w>=temp[i].first)
+                pq.push(temp[i].second);
+            }
+            if(k==0)break;
+        }
+        while(!pq.empty() && k>0){
+            w+=pq.top();
+            pq.pop();
+            k--;
+        }
+        return w;
+    }
+};
