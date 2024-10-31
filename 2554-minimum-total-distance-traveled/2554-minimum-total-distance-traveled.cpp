39c0b5b194459afce153596de434030943ba21b1
Gaurav <gaurav319288@gmail.com>
2024-10-31 21:37:58 +0530

    Time: 147 ms (20.69%), Space: 22.2 MB (41.88%) - LeetHub

---
+++ 2554-minimum-total-distance-traveled/2554-minimum-total-distance-traveled.cpp
@@ -0,0 +1,35 @@
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+class Solution {
+public:
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        sort(robot.begin(),robot.end());
+        vector<int> v;
+        for(auto i:factory){
+            int m=i[1];
+            while(m--){
+                v.push_back(i[0]);
+            }
+        }
+        long long mn=-1e12;
+        sort(v.begin(),v.end());
+        long long dp[101][v.size()+1];
+        memset(dp,-1,sizeof(dp));
+        auto get=[&](auto&&get,int i,int j)->long long{
+            if(i>=robot.size())return 0;
+            if(j>=v.size())return 1e15;
+
+            if(dp[i][j]!=-1)return dp[i][j];
+            long long ans=1e15;
+
+            ans=min(ans,get(get,i,j+1));
+            ans=min(ans,abs(robot[i]-v[j])+get(get,i+1,j+1));
+            return dp[i][j]=ans;
+        };
+        return get(get,0,0);
+    }
+};
