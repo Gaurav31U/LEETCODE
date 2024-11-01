11451181f95880cd8ec4eb45b6fc656749a23a51
Gaurav <gaurav319288@gmail.com>
2024-11-01 18:47:36 +0530

    Time: 143 ms (21.21%), Space: 22.1 MB (42.33%) - LeetHub

--- 2554-minimum-total-distance-traveled/2554-minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/2554-minimum-total-distance-traveled.cpp
@@ -1,9 +1,3 @@
-static const int _ = [](){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}();
 class Solution {
 public:
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
